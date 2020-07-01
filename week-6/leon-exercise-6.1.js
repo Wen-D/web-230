d6e4f885b922d70511020fbb92e1d10fd22f62da
Wendy Leon <pixelmamma@gmail.com>
2020-07-01 19:00:25 -0400

    Adding week 6 folder and exercise 6.1 file

---
+++ week-6/leon-exercise-6.1.js
@@ -0,0 +1,51 @@
+/* ===========================================
+; Title: Assignment 6. Objects
+; Author: Professor Krasso
+; Date: 24 July 2020
+; Modified By: Wendy Leon
+; Description: Objects
+;=========================================== */
+
+/*
+create a simple JavaScript program using
+object properties with at least (2) errors
+*/
+
+/*
+  Expected output:
+  day1: Monday
+  day2: Tuesday
+  day3: Wednesday
+  day4: Thursday
+  day5: Friday
+  day: Saturday
+  day: Sunday
+*/
+
+// Add Header
+
+var header = require('../header.js');
+console.log(header.display("Wendy", "Leon", "Exercise 6.1 - Objects"));
+console.log('\n');
+
+function prinThis () {
+
+ return ('Sunday');
+
+}
+
+var daysOfWeek = {
+   day1: 'Monday',
+   day2: 'Tuesday',
+   day3: 'Wednesday',
+   day4: 'Thursday',
+   day5: 'Friday',
+   day6: 'Saturday',
+   day7: printThis()
+}
+
+//daysOfWeek.forEach ( key => console.log ( days [ key ]) );
+
+
+
+console.log(daysOfWeek)
