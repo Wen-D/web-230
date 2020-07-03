cf4439567d16e9ab5077c64a51c0250374c5ce92
Wendy Leon <pixelmamma@gmail.com>
2020-07-03 08:35:22 -0400

    Adding header edits to 6.1 post #2

--- week-6/leon-exercise-6.2.js
+++ week-6/leon-exercise-6.2.js
@@ -20,7 +20,7 @@
 // Add Header
 
 var header = require('../header.js');
-console.log(header.display("Wendy", "Leon", "Exercise 6.2 - Exception Handling"));
+console.log(header.display("Wendy", "Leon", "Exercise 6.1 - Find the two+ errors"));
 console.log('\n');
 
 try {
